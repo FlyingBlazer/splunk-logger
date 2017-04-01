ef0bfe2c5a3aeb2815d8f1192ff78228959ab132
于湛 <me@yuzhan.info>
2017-04-01 12:30:14 +0800

    update format

--- src/SplunkLogger/SplunkFormatter.php
+++ src/SplunkLogger/SplunkFormatter.php
@@ -6,7 +6,7 @@ use Monolog\Formatter\LineFormatter;
 
 class SplunkFormatter extends LineFormatter
 {
-    const FORMAT = "[%datetime%] app=%channel% message=%message% %context%\n";
+    const FORMAT = "[%datetime%] app=%channel%, message=%message%, %context%\n";
 
     public function __construct()
     {
