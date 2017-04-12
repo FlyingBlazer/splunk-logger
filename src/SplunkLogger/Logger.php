961e9db7605deff33c34638bacf6d51b41d27a78
于湛 <me@yuzhan.info>
2017-04-12 20:41:41 +0800

    fix

--- src/SplunkLogger/Logger.php
+++ src/SplunkLogger/Logger.php
@@ -15,7 +15,7 @@ class Logger
         $log_path = env('SPLUNK_LOG_PATH', 'storage/logs/splunk.log');
 
         $this->logger = new MonoLogger($log_prefix);
-        $handler = new StreamHandler($log_path, MonoLogger::INFO);
+        $handler = new StreamHandler(base_path($log_path), MonoLogger::INFO);
         $handler->setFormatter(new SplunkFormatter());
         $this->logger->pushHandler($handler);
     }
